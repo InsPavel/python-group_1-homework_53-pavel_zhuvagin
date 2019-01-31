55587bb8fcb8bc63326c8871e71c51ee9681cf5f
pavel <zhpv07@gmail.com>
2019-01-31 15:40:36 -0500

    Добавлен файл Loto и Loto.js

---
+++ mounth_7/hw/hw_53/components-demo/src/Loto/Loto.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Loto extends Component {
+    render() {
+        return (
+            <p>{this.props.number}</p>
+        );
+    }
+}
+
+export default Loto;
