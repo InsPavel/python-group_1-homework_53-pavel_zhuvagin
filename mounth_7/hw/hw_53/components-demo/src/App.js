82586af5b226bc3804e6addfdc2734a47fbad5c6
pavel <zhpv07@gmail.com>
2019-01-31 16:08:04 -0500

    Модифицирован App.js

--- mounth_7/hw/hw_53/components-demo/src/App.js
+++ mounth_7/hw/hw_53/components-demo/src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Loto from './Loto/Loto.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    state = {
+        random: []
+    };
+
+    handleClick = () => {
+        let min = 5;
+        let max = 36;
+        let gen = () => {
+            return Math.floor((Math.random() * (max - min + 1)) + min);
+        };
+
+        let arr = [];
+
+        let inArr = (val) => {
+            for(let i=0; i <= arr.length; i++){
+                if(arr[i] === val)
+                    return true;
+            }
+        };
+
+        while(arr.length < 5){
+            let tmp = gen();
+            if(inArr(tmp))
+                continue;
+            arr.push(tmp)
+        }
+
+        function compareNumeric(a, b) {
+          return a - b;
+        }
+
+        arr.sort(compareNumeric);
+
+        this.setState({
+            random: arr
+        });
+        console.log(this.state.random);
+    };
+
+    render() {
+        return (
+        <div className="container">
+            <button onClick={this.handleClick}>Новые числа</button>
+            <div className="number">
+                <Loto number={this.state.random[0]}/>
+                <Loto number={this.state.random[1]}/>
+                <Loto number={this.state.random[2]}/>
+                <Loto number={this.state.random[3]}/>
+                <Loto number={this.state.random[4]}/>
+            </div>
+        </div>
+        )
+    }
 }
 
 export default App;
